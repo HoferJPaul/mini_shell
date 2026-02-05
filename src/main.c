914d33f85cca48e90e8e717ebfa92a36af8a8e98
suedac <zehrasuedacigdem@gmail.com>
2026-02-05 14:27:10 +0100

    handling signals and ctrl-d  now

--- src/main.c
+++ src/main.c
@@ -3,36 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 17:06:52 by phofer            #+#    #+#             */
-/*   Updated: 2026/02/04 17:59:25 by phofer           ###   ########.fr       */
+/*   Updated: 2026/02/05 13:58:13 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../include/minishell.h"
 #include "../include/parser.h"
-
-int			g_exit_status = 0;
-
-//Handles SIGINT (Ctrl+C is printed and ignored)
-static void	sigint_handler(int signo)
-{
-	(void)signo;
-	g_exit_status = (130);
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-}
-
-//Sets up signal handlers: SIGINT → custom handler, SIGQUIT → ignored.
-static void	setup_signals(void)
-{
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, SIG_IGN);
-}
 
 //initiates parsing/tokenizing functions
 // located in ../src/parser
@@ -43,6 +23,7 @@ static void	setup_signals(void)
 // 	// expand_tokens();
 // 	// parse_tokens();
 // }
+int			g_exit_status = 0;
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -55,11 +36,7 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		input = readline("minishell> ");
-		if (!input)
-		{
-			printf("exit\n");
-			break ;
-		}
+		ctrl_d(input);
 		if (*input)
 		{
 			add_history(input);
--- src/utils/handle_signals.c
+++ src/utils/handle_signals.c
@@ -3,14 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   handle_signals.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 12:08:33 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/02/04 17:23:41 by phofer           ###   ########.fr       */
+/*   Updated: 2026/02/05 13:59:10 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 //ctrl-c, ctrl-d, ctrl-'\' handling functions here
+//Handles SIGINT (Ctrl+C is printed and ignored)
+void	sigint_handler(int signo)
+{
+	(void)signo;
+	g_exit_status = (130);
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 
+//Sets up signal handlers: SIGINT → custom handler, SIGQUIT → ignored.
+void	setup_signals(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+void    ctrl_d(char *line)//technically this is not a signal we can catch, so we are catching the EOF
+{
+    if(!line)
+    {
+        printf("exit\n");
+        //cleanup stuff here
+        exit(0);
+    }
+}
