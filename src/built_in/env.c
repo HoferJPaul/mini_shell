a6cf14a5577e71842b7c12bb4c91364084ebcc2d
Zehra Sueda Gahramanli <zgahrama@c2r10s3.42prague.com>
2026-02-06 12:44:18 +0100

    env builtin func added

--- src/built_in/env.c
+++ src/built_in/env.c
@@ -6,8 +6,21 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:55:19 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/02/05 14:56:24 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/02/06 12:39:54 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+//already tested as a function but need to re-test it when the parser is ready :D
+void env(char **envp)
+{
+    char **new_env = envp;
+    
+    while (*new_env != 0)
+    {
+        char *thisEnv = *new_env;
+        printf("%s\n", thisEnv);
+        new_env++;
+    }
+}
--- src/utils/cleanup.c
+++ src/utils/cleanup.c
@@ -6,10 +6,11 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 12:09:21 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/02/04 12:09:49 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/02/05 17:27:22 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 //all the frees etc here
+//should free the add_history with clear_history here
