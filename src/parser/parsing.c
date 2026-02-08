932d867d2467039564f7befe3b09e4e1edab727a
Paul Hofer <phofer@student.42prague.com>
2026-02-08 15:30:43 +0100

    updated lex/parsing

--- src/parser/parsing.c
+++ src/parser/parsing.c
@@ -6,24 +6,83 @@
 /*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 16:32:40 by phofer            #+#    #+#             */
-/*   Updated: 2026/02/08 13:36:59 by phofer           ###   ########.fr       */
+/*   Updated: 2026/02/08 15:26:00 by phofer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/tokens.h"
 
-t_token_type	get_operator_type(const char *s)
+//allocates a new token
+t_token	*new_token(t_token_type type, const char *str, size_t len)
 {
+	t_token	*token;
+
+	if (!str)
+		return (NULL);
+	token = malloc(sizeof(*token));
+	if (!token)
+		return (NULL);
+	token->value = ft_substr(str, 0, len);
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
+
+//Adds token to linked list
+void	add_token(t_token **head, t_token *new)
+{
+	t_token	*temp;
+
+	if (!new)
+		return ;
+	if (!*head)
+		*head = new;
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
+}
+
+//determines operator type and len
+t_token_type	get_operator_info(const char *s, size_t *len)
+{
+	if (!s || !len)
+		return (T_NONE);
 	if (s[0] == '<' && s[1] == '<')
-		return (T_HEREDOC);
+		return (*len = 2, T_HEREDOC);
 	if (s[0] == '>' && s[1] == '>')
-		return (T_APPEND);
+		return (*len = 2, T_APPEND);
+	if (s[0] == '|')
+		return (*len = 1, T_PIPE);
 	if (s[0] == '<')
-		return (T_REDIR_IN);
+		return (*len = 1, T_REDIR_IN);
 	if (s[0] == '>')
-		return (T_REDIR_OUT);
-	return (T_PIPE);
+		return (*len = 1, T_REDIR_OUT);
+	return (*len = 0, T_NONE);
+}
+//handles operator
+int tokenize_operator(t_token **tokens, const char *str, size_t *i)
+{
+	t_token_type	type;
+	size_t			len;
+
+	if (!str || !i)
+		return (-1);
+	type = get_operator_info(&str[*i], &len);
+	if (type == T_NONE)
+		return (-1);
+	add_token(tokens, new_token(type, &str[*i], len));
+	*i += len;
+	return (0);
 }
 
 int	is_quote(char c)
@@ -51,18 +110,21 @@ int	lex_line(t_shell *mini, const char *input)
 
 	mini->tokens = NULL;
 	i = 0;
-	while (input[i] && input[i] != '\n')
+	while (input[i] || input[i] != '\n')
 	{
 		skip_whitespaces(input, &i);
 		if (!input[i] && input[i] != '\n')
 			break ;
 		if (is_operator(input[i]))
-			tokenize_operator(mini->tokens, input, &i); //todo
-		else if (!tokenize_word(mini->tokens, input, &i))
 		{
-			//print error;
-			return (0);
+			if (tokenize_operator(&mini->tokens, input, &i) != 0)
+				return (-1);
+		}
+		else if (tokenize_word(mini->tokens, input, &i) != 0) //todo
+		{
+			ft_putstr_fd("error: lex line\n", 2);
+			return (1);
 		}
 	}
-	return (1);
+	return (0);
 }
