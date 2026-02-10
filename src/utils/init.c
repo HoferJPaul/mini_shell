92c9b3fffddaa8f35161d3a9377b7f853a3cbae1
Zehra Sueda Gahramanli <zgahrama@c2r10s3.42prague.com>
2026-02-10 16:50:07 +0100

    created path functions. version 1

--- src/utils/init.c
+++ src/utils/init.c
@@ -6,7 +6,7 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:19:14 by phofer            #+#    #+#             */
-/*   Updated: 2026/02/10 13:16:59 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/02/10 14:34:09 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ void	process_line(t_shell *mini, char *input)
 //assuming we already initialized env and env pointer in mini struct already points at created env.
 int setup_struct(t_shell *mini, char **envp, t_env *env)
 {
-	char *path_value;
-
     if (!mini || !env)
         return (0);
 	(void)envp;//we don't really need to get this as an argument if we already copied the envp to our copy.
@@ -33,15 +31,7 @@ int setup_struct(t_shell *mini, char **envp, t_env *env)
     mini->flag = 0;
     mini->g_exit_status = 0;
     mini->running = 1;
-    
-    path_value = env_get(mini->env, "PATH");
-    if (path_value)
-        mini->paths = ft_split(path_value, ':');
-    else
-        mini->paths = NULL;
-		//cleanup the initialized stuff(?)
-    if (path_value && !mini->paths)
-        return (0);
+	init_paths(env, mini);
     mini->cwd = getcwd(NULL, 0);
     if (!mini->cwd)
         return (0);
