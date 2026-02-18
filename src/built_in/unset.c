5e950b5d12b17d652e4a335afe57c8f58b5a81b6
Zehra Sueda Gahramanli <zgahrama@c2r10s3.42prague.com>
2026-02-18 12:30:04 +0100

    unset helper created

--- src/built_in/unset.c
+++ src/built_in/unset.c
@@ -6,8 +6,34 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:53:52 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/02/05 14:55:30 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/02/16 17:08:21 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+static int validate_var(char *var)
+{
+     int i;
+
+    if (var[0] && (var[0] == '_' || ft_isalpha(var[0]) == 1))
+    {
+        i = 1;
+        while (var[i])
+        {
+            if (var[i] != '_' && ft_isalnum(var[i]) != 1)
+            {
+                ft_putstr_fd("export: not a valid identifier\n", 2);
+                return -1;
+            }
+            i++;
+        }
+        return 1;
+    }
+    ft_putstr_fd("export: not a valid identifier\n", 2);
+    return -1;
+}
+//should return 0 on success and 1 on fail.
+int unset(t_env *env, char **vars)
+{
+    
+}
