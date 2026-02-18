c021ca7db69c462951525f58955d3ccac295f352
Zehra Sueda Gahramanli <zgahrama@c2r10s3.42prague.com>
2026-02-18 16:08:14 +0100

    unset done with tests

--- src/built_in/unset.c
+++ src/built_in/unset.c
@@ -6,7 +6,7 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:53:52 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/02/16 17:08:21 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/02/18 15:45:52 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,84 @@ static int validate_var(char *var)
         while (var[i])
         {
             if (var[i] != '_' && ft_isalnum(var[i]) != 1)
-            {
-                ft_putstr_fd("export: not a valid identifier\n", 2);
-                return -1;
-            }
+                return 0;
             i++;
         }
         return 1;
     }
-    ft_putstr_fd("export: not a valid identifier\n", 2);
-    return -1;
+    return 0;
+}
+static int check_var_exists(t_env **env, char *var)
+{
+    t_env *curr = *env;
+    size_t len = ft_strlen(var);
+
+    while (curr)
+    {
+        if (ft_strncmp(curr->key, var, len) == 0
+            && curr->key[len] == '\0')
+            return 1;
+        curr = curr->next;
+    }
+    return 0;
+}
+static void remove_var(t_env **env, char *var)
+{
+    t_env *curr;
+    t_env *prev;
+    t_env *next;
+    size_t len;
+
+    curr = *env;
+    prev = NULL;
+    len = ft_strlen(var);
+    while (curr)
+    {
+        next = curr->next;
+
+        if (ft_strncmp(curr->key, var, len) == 0
+            && curr->key[len] == '\0')
+        {
+            if (prev)
+                prev->next = next;
+            else
+                *env = next;
+
+            free(curr->key);
+            free(curr->value);
+            free(curr);
+        }
+        else
+            prev = curr;
+        curr = next;
+    }
 }
 //should return 0 on success and 1 on fail.
-int unset(t_env *env, char **vars)
+int unset(t_env **env, char **vars)
 {
-    
+    int i;
+
+    if(!vars)
+    {
+        ft_putstr_fd("unset: no arguments\n", 2);
+        return 1;//fail
+    }
+    i = 0;
+    while(vars[i])
+    {
+        if(!validate_var(vars[i]))
+        {
+            ft_putstr_fd("unset: not a valid identifier\n", 2);
+            i++;
+            continue;
+        }
+        if(!check_var_exists(env, vars[i]))
+        {
+            i++;
+            continue;
+        }
+        remove_var(env, vars[i]);
+        i++;
+    }
+    return 0;//success
 }
