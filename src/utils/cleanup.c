ec35d1391d961b9561485d7556509bd301550c0c
suedac <zehrasuedacigdem@gmail.com>
2026-02-23 10:50:33 +0100

    deletion of an unuseful comment

--- src/utils/cleanup.c
+++ src/utils/cleanup.c
@@ -6,13 +6,12 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 12:09:21 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/02/10 14:52:10 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/02/20 12:13:18 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-//all the frees etc here
 //should free the add_history with clear_history here
 void free_array(char **arr)
 {
