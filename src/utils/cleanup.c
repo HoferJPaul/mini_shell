296e599d7649b877691bf62833184a5724554567
suedac <zehrasuedacigdem@gmail.com>
2026-03-05 16:02:56 +0100

    update.txt

--- src/utils/cleanup.c
+++ src/utils/cleanup.c
@@ -6,14 +6,14 @@
 /*   By: zgahrama <zgahrama@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 12:09:21 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/03/05 13:39:43 by zgahrama         ###   ########.fr       */
+/*   Updated: 2026/03/05 14:20:29 by zgahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 //should free the add_history with clear_history here
-void free_array(char **arr)
+void free_array(char **arr)//use it for mini->paths
 {
     int i = 0;
 
@@ -23,14 +23,19 @@ void free_array(char **arr)
         free(arr[i++]);
     free(arr);
 }
-/*void free_env(t_env *env)
+void free_env_nodes(t_env *env)
 {
     t_env *curr;
-
-    while(env != NULL)
+    t_env *next;
+    
+    curr = env;
+    while(curr != NULL)
     {
-        env = curr;
-        env = env->next;
+        next = curr->next;
+        free(curr->key);
+        if(curr->value)
+            free(curr->value);
         free(curr);
+        curr = next;
     }
-}*/
+}
