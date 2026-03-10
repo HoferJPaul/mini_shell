c58cbc1430e07578f92053ff46dd3d25e3c693be
Paul Hofer <phofer@student.42prague.com>
2026-03-10 14:57:18 +0100

    norm fixes

--- src/built_in/cd.c
+++ src/built_in/cd.c
@@ -6,72 +6,87 @@
 /*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:52:22 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/03/09 18:51:25 by phofer           ###   ########.fr       */
+/*   Updated: 2026/03/10 14:52:33 by phofer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/tokens.h"
 
-static char *cd_resolve_target(t_shell *mini, char **command)
+// Returns HOME env var, or NULL with error.
+static char	*cd_get_home(t_shell *mini)
 {
-    char *target;
+	char	*target;
 
-    if (!command[1])
-    {
-        target = env_get(mini->env, "HOME");
-        if (!target)
-        {
-            printf("cd: HOME not set\n");
-            return NULL;
-        }
-        return target;
-    }
-    if (command[2])
-    {
-        ft_putendl_fd("cd: too many arguments\n", STDERR_FILENO);
-        return NULL;
-    }
-    else if (ft_strcmp(command[1], "-") == 0)
-    {
-        target = env_get(mini->env, "OLDPWD");
-        if (!target)
-        {
-            ft_putendl_fd("cd: OLDPWD not set\n", STDERR_FILENO);
-            return NULL;
-        }
-        return(target);
-    }
-    return (command[1]);
+	target = env_get(mini->env, "HOME");
+	if (!target)
+	{
+		printf("cd: HOME not set\n");
+		return (NULL);
+	}
+	return (target);
 }
 
-static int cd_change_directory(char *target)
+/*
+** Resolves the cd target path from command args:
+** - No arg      -> $HOME
+** - "-"         -> $OLDPWD
+** - Too many    -> error, NULL
+** - Otherwise   -> command[1]
+*/
+static char	*cd_resolve_target(t_shell *mini, char **command)
 {
-    if (chdir(target) != 0)
-    {
-        perror("cd");
-        return (1);
-    }
-    return (0);
+	char	*target;
+
+	if (!command[1])
+		return (cd_get_home(mini));
+	if (command[2])
+	{
+		ft_putendl_fd("cd: too many arguments\n", STDERR_FILENO);
+		return (NULL);
+	}
+	if (ft_strcmp(command[1], "-") == 0)
+	{
+		target = env_get(mini->env, "OLDPWD");
+		if (!target)
+		{
+			ft_putendl_fd("cd: OLDPWD not set\n", STDERR_FILENO);
+			return (NULL);
+		}
+		return (target);
+	}
+	return (command[1]);
 }
 
-static int cd_update_pwd_vars(t_shell *mini, char *oldpwd)
+// Calls chdir(); prints error and returns 1 on failure.
+static int	cd_change_directory(char *target)
 {
-    char *new_pwd;
+	if (chdir(target) != 0)
+	{
+		perror("cd");
+		return (1);
+	}
+	return (0);
+}
 
-    new_pwd = getcwd(NULL, 0);
-    if (!new_pwd)
-    {
-        perror("cd");
-        return 1;
-    }
-    env_set(&mini->env, "OLDPWD", oldpwd, 0);
-    env_set(&mini->env, "PWD", new_pwd, 0);
-    free(mini->cwd);
-    mini->cwd = ft_strdup(new_pwd);
-    free(new_pwd);
-    free(oldpwd);
-    return 0;
+// Updates OLDPWD, PWD and mini->cwd after a successful chdir().
+static int	cd_update_pwd_vars(t_shell *mini, char *oldpwd)
+{
+	char	*new_pwd;
+
+	new_pwd = getcwd(NULL, 0);
+	if (!new_pwd)
+	{
+		perror("cd");
+		return (1);
+	}
+	env_set(&mini->env, "OLDPWD", oldpwd, 0);
+	env_set(&mini->env, "PWD", new_pwd, 0);
+	free(mini->cwd);
+	mini->cwd = ft_strdup(new_pwd);
+	free(new_pwd);
+	free(oldpwd);
+	return (0);
 }
 /*
 ** cd builtin:
@@ -80,29 +95,29 @@ static int cd_update_pwd_vars(t_shell *mini, char *oldpwd)
 ** 3) chdir() to target; on failure, print error and keep cwd unchanged.
 ** 4) On success, update OLDPWD/PWD in env and mini->cwd.
 */
-int cd(t_shell *mini, char **command)
+int	cd(t_shell *mini, char **command)
 {
-    char	*target;
-    char	*old_pwd;
+	char	*target;
+	char	*old_pwd;
 
-    target = cd_resolve_target(mini, command);
-    if (!target)
-        return (mini->g_exit_status = 1);
-    old_pwd = getcwd(NULL, 0);
-    if (!old_pwd)
-    {
-        perror("cd: getcwd");
-        return (mini->g_exit_status = 1);
-    }
-    if (cd_change_directory(target) != 0)
-    {
-        free(old_pwd);
-        return (mini->g_exit_status = 1);
-    }
-    if(cd_update_pwd_vars(mini, old_pwd) == 1)
-    {
-        free(old_pwd);
-        return(mini->g_exit_status = 1);
-    }
-    return (mini->g_exit_status = 0);
+	target = cd_resolve_target(mini, command);
+	if (!target)
+		return (mini->g_exit_status = 1);
+	old_pwd = getcwd(NULL, 0);
+	if (!old_pwd)
+	{
+		perror("cd: getcwd");
+		return (mini->g_exit_status = 1);
+	}
+	if (cd_change_directory(target) != 0)
+	{
+		free(old_pwd);
+		return (mini->g_exit_status = 1);
+	}
+	if (cd_update_pwd_vars(mini, old_pwd) == 1)
+	{
+		free(old_pwd);
+		return (mini->g_exit_status = 1);
+	}
+	return (mini->g_exit_status = 0);
 }
--- src/built_in/echo.c
+++ src/built_in/echo.c
@@ -6,13 +6,29 @@
 /*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:52:09 by zgahrama          #+#    #+#             */
-/*   Updated: 2026/03/09 18:30:38 by phofer           ###   ########.fr       */
+/*   Updated: 2026/03/10 14:57:05 by phofer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/tokens.h"
 
+// Prints args separated by spaces, no newline.
+static void	echo_print_args(char **command, int i)
+{
+	while (command[i])
+	{
+		ft_putstr_fd(command[i], STDOUT_FILENO);
+		if (command[i + 1])
+			ft_putstr_fd(" ", STDOUT_FILENO);
+		i++;
+	}
+}
+
+/*
+** echo builtin. Prints command args to stdout separated by spaces.
+** Supports -n flag (suppress trailing newline). No arg prints newline.
+*/
 int	echo(char **command)
 {
 	int	flag;
@@ -31,13 +47,7 @@ int	echo(char **command)
 			ft_putstr_fd("\n", STDOUT_FILENO);
 		return (0);
 	}
-	while (command[i])
-	{
-		ft_putstr_fd(command[i], STDOUT_FILENO);
-		if (command[i + 1])
-			ft_putstr_fd(" ", STDOUT_FILENO);
-		i++;
-	}
+	echo_print_args(command, i);
 	if (flag == 0)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (0);
